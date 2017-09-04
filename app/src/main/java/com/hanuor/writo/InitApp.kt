97c6dac00ce58c6702afa42143218ee963a1ce04
hanuor <hanuortech@gmail.com>
2017-09-04 22:29:24 +0530

    Application class

---
+++ app/src/main/java/com/hanuor/writo/InitApp.kt
@@ -0,0 +1,17 @@
+package com.hanuor.writo
+
+import android.app.Application
+
+/**
+ * Created by Shantanu Johri on 04-09-2017.
+ */
+class InitApp: Application(){
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+    }
+
+}
