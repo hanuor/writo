61ecef801f3d17c076bae1f07b45e2b8e0faeae3
hanuor <hanuortech@gmail.com>
2017-09-05 10:15:05 +0530

    Removed unnecassary SDK files

--- app/libs/App42_ANDROID_SDK-master/sample/App42-Android-SampleAPITest/README.md
+++
@@ -1,4 +0,0 @@
-App42_ANDROID_API
-=================
-
-App42 Android Client SDK
--- app/libs/App42_ANDROID_SDK-master/sample/App42-Android-SampleAPITest/gen/com/app42/api/test/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.app42.api.test;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
